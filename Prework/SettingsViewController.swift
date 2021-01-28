2230c5ffc85baab299205315be80ed953cdef9ef
Laura Hernandez <lvhernndez@gmail.com>
2021-01-28 13:31:30 -0800

    finished settings and percentages update on Settings view and tip control

--- Prework/SettingsViewController.swift
+++ Prework/SettingsViewController.swift
@@ -8,14 +8,6 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
     /*
     // MARK: - Navigation
 
@@ -25,5 +17,49 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBOutlet weak var percentage1Label: UITextField!
+    @IBOutlet weak var percentage2Label: UITextField!
+    @IBOutlet weak var settingsView: UIView!
+    @IBOutlet weak var percentage3Label: UITextField!
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        settingsView.layer.cornerRadius = 10
+        
+        
+        let defaults = UserDefaults.standard
+        let per1 = defaults.string(forKey: "percentage1") ?? "15"
+        let per2 = defaults.string(forKey: "percentage2") ?? "18"
+        let per3 = defaults.string(forKey: "percentage3") ?? "20"
+        defaults.synchronize()
+        percentage1Label.text = per1
+        percentage2Label.text = per2
+        percentage3Label.text = per3
+    }
+
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    
+    @IBAction func percentage1Changed(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(percentage1Label.text, forKey: "percentage1")
+        defaults.synchronize()
+    }
+    @IBAction func percentage2Changed(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(percentage2Label.text, forKey: "percentage2")
+        defaults.synchronize()
+    }
+    @IBAction func percentage3Changed(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(percentage3Label.text, forKey: "percentage3")
+        defaults.synchronize()
+    }
+    
 
 }
--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -20,16 +20,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var perPersonLabel: UILabel!
     @IBOutlet weak var partySizeControl: UIStepper!
     @IBOutlet weak var partySizeLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         billAmountView.layer.cornerRadius = 10
         totalView.layer.cornerRadius = 10
         partySizeControl.minimumValue = 1
         partySizeControl.maximumValue = 50
+        
+        let defaults = UserDefaults.standard
+        let per1 = defaults.string(forKey: "percentage1") ?? "15"
+        let per2 = defaults.string(forKey: "percentage2") ?? "18"
+        let per3 = defaults.string(forKey: "percentage3") ?? "20"
+        // not sending the right numbers from the settings view!
+        tipControl.setTitle(String(per1 + "%"), forSegmentAt: 0)
+        tipControl.setTitle(String(per2 + "%"), forSegmentAt: 1)
+        tipControl.setTitle(String(per3 + "%"), forSegmentAt: 2)
+
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
        
     }
     @IBAction func calculateTip(_ sender: Any) {
+        
         //1. Get bill amount from text field
         let bill = Double(billAmountTextField.text!) ?? 0
         // Get party size
